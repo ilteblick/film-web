5359526fc578fdd05a8924ecab7ec9279a9723dc
ilteblick <deniskochetkov1995@gmail.com>
2016-11-16 20:29:20 +0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to film-web
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/film_web/core_test.clj
@@ -0,0 +1,7 @@
+(ns film-web.core-test
+  (:require [clojure.test :refer :all]
+            [film-web.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
